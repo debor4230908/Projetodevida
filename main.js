90a54e1495eb2c64f7fd6f87327caa92bf9fed68
debor4230908 <debora.rodrigues.araujo@escola.pr.gov.br>
2025-06-18 12:25:02 -0300

    Add files via upload
    17062025

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ function diminuirTamanho(){
         tamanhoSenha--;
     }
     numerosenha.textContent = tamanhoSenha;
+    geraSenha();
 };
 
 function aumentaTamanho(){
@@ -18,4 +19,20 @@ function aumentaTamanho(){
         tamanhoSenha++;
     }
     numerosenha.textContent = tamanhoSenha;
+    geraSenha();
 };
+
+const campoSenha = document.querySelector('#campo-senha');
+const LetrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+geraSenha();
+
+function geraSenha(){
+    let senha = ' ';
+    for(let i = 0; i< tamanhoSenha; i++){
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
+    }
+    campoSenha.value = senha;
+}
